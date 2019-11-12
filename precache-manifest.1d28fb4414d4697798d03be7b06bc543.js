5ec8f9fa38c650f50dfc111cc7e518b253faa127
vasiliy gladishev <gladishevvasiliy@gmail.com>
2019-11-13 00:51:47 +0300

    Updates

---
+++ precache-manifest.1d28fb4414d4697798d03be7b06bc543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb68ca589a7f5077f2428aa8f005ddb7",
+    "url": "/add-description-app/index.html"
+  },
+  {
+    "revision": "303e3ca193186c79d573",
+    "url": "/add-description-app/static/css/main.6abd79f8.chunk.css"
+  },
+  {
+    "revision": "f3d6f597dc9e703bbe82",
+    "url": "/add-description-app/static/js/2.d3e36691.chunk.js"
+  },
+  {
+    "revision": "303e3ca193186c79d573",
+    "url": "/add-description-app/static/js/main.fcd2209a.chunk.js"
+  },
+  {
+    "revision": "b8ca2228b639dc5793bd",
+    "url": "/add-description-app/static/js/runtime-main.ee92e3a3.js"
+  },
+  {
+    "revision": "a204d2e75b6fdf6de1a13c97179e3dfc",
+    "url": "/add-description-app/static/media/Kruk-Kalashn-edit-VG.a204d2e7.ttf"
+  },
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/add-description-app/static/media/Loading.f4404720.gif"
+  }
+]);
